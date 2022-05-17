18c53bfe2a6dab31f6789c7fb4fa8dea9e0cbf38
clintonmekwunye <mekwunyeclinton@gmail.com>
2022-05-17 13:22:07 +0100

    minor changes

--- hello.py
+++ hello.py
@@ -23,10 +23,19 @@ class Person(db.Model):
 # clinton = Person(id=1,name="clinton")
 # db.session.add(clinton)
 # db.session.commit()
-    
+
+# the lines of code commented out just above this were
+# used to insert values into the persons table of the database
+# they are commented out to avoid getting error for trying to 
+# insert duplicate values into the table.
+# if its the first time running this script please undo the comment.
+
 db.create_all()
+# this ensures the database is created
+
 
 person = Person.query.first()
+
 
 @app.route('/')
 def index():
