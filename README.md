3008a446a685d341732e35b74a406c71704d10ff
clintonmekwunye <mekwunyeclinton@gmail.com>
2022-05-17 13:09:26 +0100

    the hello app on sql basics using flask

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 This repository is for my practice on the AlX-T 
-full stack nanodegree program.
+full stack nano degree program.
 
+the names of the packages differ from those in the course
+name of package in this repository - name of packcage in the coure
+1) psycopg - demo.py
---
+++ hello.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)  # instantiating the application
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:1111@localhost:5432/myordersystemdb"
+# the format for the database uri is as seen below
+# Dialect://username:password@url:port/database_name
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Person(db.Model):
+    __tablename__ = "persons"  # this line gives the name to our table
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        """this method is created to help us modify the output of 
+        our query. it helps during debuging
+        """
+        return f"<person| id:{self.id}, name:{self.name}"
+    
+# clinton = Person(id=1,name="clinton")
+# db.session.add(clinton)
+# db.session.commit()
+    
+db.create_all()
+
+person = Person.query.first()
+
+@app.route('/')
+def index():
+    return "Hello " + person.name
+
+if __name__ == '__main__':
+    app.run()
